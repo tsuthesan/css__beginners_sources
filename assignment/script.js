b7c7cd9301c4553d458ba11a6fbe88ccfe9757ae
tsuthesan <tsuthesan1996@gmail.com>
2018-04-11 13:45:21 +0530

    Add files via upload

---
+++ assignment/script.js
@@ -0,0 +1,10 @@
+a = 10;
+b = a;
+document.write('(a = 10) b = a ; b =' + b + '<br/>');
+var c = 5;
+c += a; // c = c + a;
+document.write("(a = 10, c = 5) c += a ; c =");
+document.write(c);
+//document.write('<br/>');
+
+//document.write("(a = 10, c = 5) c += a ; c =" + c + '<br/>');
