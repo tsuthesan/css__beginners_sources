2cbeb9ca9a35206499c4f776fbd167f5e1e308f0
tsuthesan <tsuthesan1996@gmail.com>
2018-04-11 13:43:04 +0530

    Add files via upload

---
+++ basic/script.js
@@ -0,0 +1,7 @@
+window.alert('Iam Alert');
+//document.write('Iam Write');
+console.log('Iam Console log');
+
+function test() {
+    //   document.getElementById("demo").innerHTML = 'Iam inner html';
+}
