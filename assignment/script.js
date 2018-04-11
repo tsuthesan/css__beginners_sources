9c2049a1e9073eae4b0643c24882153a832f274d
tsuthesan <tsuthesan1996@gmail.com>
2018-04-11 13:50:18 +0530

    Update script.js

--- assignment/script.js
+++ assignment/script.js
@@ -5,6 +5,6 @@ var c = 5;
 c += a; // c = c + a;
 document.write("(a = 10, c = 5) c += a ; c =");
 document.write(c);
-//document.write('<br/>');
+document.write('<br/>');
 
-//document.write("(a = 10, c = 5) c += a ; c =" + c + '<br/>');
+document.write("(a = 10, c = 5) c += a ; c =" + c + '<br/>');
