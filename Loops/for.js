c662d6b6d09289f0e0abe25fe78a0f9e0456db0f
tsuthesan <tsuthesan1996@gmail.com>
2018-04-15 21:37:43 +0530

    Update for.js

--- Loops/for.js
+++ Loops/for.js
@@ -3,3 +3,11 @@ for (var d = 0; d < c.length; d++) {
     document.write(c[d]);
     document.write('<br>');
 }
+
+
+
+//another for loop
+for(var i=0; i<=10; i+2)
+{
+    document.write(i+"<br/>")
+}
